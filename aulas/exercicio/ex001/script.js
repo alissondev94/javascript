2b2fe4a690b671f575902490bb60d8053f59f023
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-10-11 13:41:34 -0300

    ex hora do dia

--- aulas/exercicio/ex001/script.js
+++ aulas/exercicio/ex001/script.js
@@ -7,14 +7,14 @@ function carregar() {
     if (hora >= 0 && hora < 12) {
         //Bom dia!
         img.src = 'manhapq.png'
-        document.body.style.background ='#485059'
+        document.body.style.background ='#e2cd9f'
     } else if (hora >= 12 && hora < 18) {
         //Boa tarde!
         img.src = 'tardepq.png'
-        document.body.style.background ='#F2AB6D'
+        document.body.style.background ='#b9846f'
     } else {
         //Boa noite!
         img.src = 'noitepq.png'
-        document.body.style.background ='#1D3240'
+        document.body.style.background ='#515154'
     }
 }
