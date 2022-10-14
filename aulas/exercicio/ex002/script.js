b78282c6e11ae5bbd06f6a0dd33c56af859ab8d6
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-10-14 10:40:32 -0300

    Update script.js

--- aulas/exercicio/ex002/script.js
+++ aulas/exercicio/ex002/script.js
@@ -9,12 +9,41 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id' , 'foto')
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >=0 && idade <15){
+                //criança
+                img.setAttribute('src', 'criancam.png')
+            } else if (idade <30){
+                //Jovem
+                img.setAttribute('src', 'jovemm.png')
+            } else if (idade <50){
+                //Adulto
+                img.setAttribute('src', 'adultom.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'idosom.png')
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >=0 && idade <15){
+                //criança
+                img.setAttribute('src', 'criancaf.png')
+            } else if (idade <30){
+                //Jovem
+                img.setAttribute('src', 'jovemf.png')
+            } else if (idade <50){
+                //Adulto
+                img.setAttribute('src', 'adultof.png')
+            } else {
+                //Idoso
+                img.setAttribute('src', 'idosof.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
