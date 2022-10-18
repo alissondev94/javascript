afd84b093384931b3b3224e915a555f98a3b404f
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-10-18 10:25:21 -0300

    Create ex001.js

---
+++ aulas/repetiçoes/ex001.js
@@ -0,0 +1,12 @@
+var contador = 1
+while (contador <= 10){
+    console.log(`${contador}`)
+    contador++
+}
+// estruturas diferentes com mesmo resultado
+
+var contar = 1
+do{
+    console.log(`${contar}`)
+    contar++
+} while(contar <=10)
