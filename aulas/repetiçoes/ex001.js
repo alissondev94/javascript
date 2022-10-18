f983a5e88ed0ab0a121ed4ed38c4c7bc45ece2f1
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-10-18 11:35:43 -0300

    Update ex001.js

--- aulas/repetiçoes/ex001.js
+++ aulas/repetiçoes/ex001.js
@@ -1,12 +1,19 @@
+/*Estrutura de repetição WHILE*/
 var contador = 1
 while (contador <= 10){
     console.log(`${contador}`)
     contador++
 }
-// estruturas diferentes com mesmo resultado
 
+/*Estrutura de repetição DO*/
 var contar = 1
 do{
     console.log(`${contar}`)
     contar++
 } while(contar <=10)
+
+
+/*Estrutura de repetição FOR*/
+for(var conte=1; conte<=10;conte++){
+    console.log(`${conte}`)
+}
