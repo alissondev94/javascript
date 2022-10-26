0a3d950bb7cfc399bf522cf503283a9554f47b80
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-10-26 10:02:25 -0300

    Super contador

--- aulas/exercicio/ex003/script.js
+++ aulas/exercicio/ex003/script.js
@@ -1,1 +1,32 @@
-function contar
+function contar() {
+    let ini = document.getElementById('txtinicio')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpasso')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[Erro] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: \u{1F449}'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passo invalido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if(i < f) {
+            //contagem crescente
+            for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
