9e9f6406ae035efb8b2c48784bc508ab51a3d3dc
Alisson Paixão <102302616+alissondev94@users.noreply.github.com>
2022-11-02 10:40:55 -0300

    funçoes

---
+++ aulas/funções/ex001.js
@@ -0,0 +1,12 @@
+function parimpar(n){
+    if (n % 2 == 0 ){
+        return 'O numero é par!'
+    } else {
+        return 'O numero é impar!'
+    }
+}
+
+/* var res = parimpar(4)
+console.log(`O numero é ${res}!`)*/
+
+console.log(parimpar(25))
